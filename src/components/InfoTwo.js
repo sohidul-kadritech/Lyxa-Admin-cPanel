c1b72619cd9b39dd595e587cb83207c54cda8970
nazib-kadritech <nazib.absar@kadritech.se>
2023-02-07 20:00:58 +0600

    style: add new info style

--- src/components/InfoTwo.js
+++ src/components/InfoTwo.js
@@ -3,36 +3,34 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
-const InfoTwo = ({ value, Icon, link, mapLink, classes }) => {
+const InfoTwo = ({ name, value, Icon, link, mapLink, classes }) => {
   const history = useHistory();
   let check = Array.isArray(value);
+
   if (!check) {
     return (
       <InfoWrapper
-        className={`${link && "cursor-pointer"}  }`}
+        className={`${link && "cursor-pointer"}}`}
         onClick={() => history.push(link)}
         link={link}
         mapLink={mapLink}
       >
         <Icon className="text-danger" />
-        <Tooltip
-          title={`${link ? "See details" : mapLink ? "See Location" : ""}`}
-        >
+        <Tooltip title={`${link ? "See details" : mapLink ? "See Location" : ""}`}>
           {mapLink ? (
-            <a
-              className={`value ${classes || ""}`}
-              href={mapLink}
-              target="blank"
-            >
-              {value}
+            <a className={`item-wrap`} href={mapLink} target="blank">
+              <span className="name">{name}:</span>
+              <span className={`value ${classes}`}>{value}:</span>
             </a>
           ) : (
-            <span className={`value ${classes || ""}`}>{value}</span>
+            <span className={`item-wrap`}>
+              <span className="name">{name}:</span>
+              <span className={`value ${classes}`}>{value}</span>
+            </span>
           )}
         </Tooltip>
       </InfoWrapper>
     );
-    
   } else {
     return (
       <InfoWrapper
@@ -62,24 +60,38 @@ const InfoWrapper = styled.div`
   padding-bottom: 8px;
   &:hover {
     text-decoration: ${({ link }) => link && "underline"};
-
     .value {
       color: ${({ link, mapLink }) => (link || mapLink ? "green" : "black")};
-      font-weight: ${({ link, mapLink }) =>
-        (link || mapLink) && "500 !important"};
+      font-weight: ${({ link, mapLink }) => (link || mapLink) && "500 !important"};
       text-decoration: ${({ mapLink }) => mapLink && "underline !important"};
     }
   }
-
   .img {
     font-size: 20px;
     color: #e95b5b;
     margin-right: 10px;
   }
-  .value {
+  .item-wrap {
+    display: inline-flex;
+    align-items: baseline;
+    justify-items: flex-start;
+    gap: 3px;
     font-size: 14px;
-    text-transform: capitalize;
-    font-weight: 500;
-    color: #6b6b6b;
+    padding-left: 4px;
+
+    .name {
+      text-transform: capitalize;
+      font-weight: 500;
+      color: rgba(0, 0, 0, 0.75);
+      font-size: 16px;
+      font-weight: bold,
+    }
+
+    .value {
+      font-size: 14px;
+      text-transform: capitalize;
+      font-weight: 500;
+      color: #6b6b6b;
+    }
   }
 `;
