756602b8776edd0aaa88f0ebdad31ba701f61cf1
nazib-kadritech <nazib.absar@kadritech.se>
2023-02-23 15:57:35 +0600

    feat: add default styling for tab panel

--- src/components/TabPanel.js
+++ src/components/TabPanel.js
@@ -1,11 +1,16 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 // mui
-import { Box } from '@mui/material';
+import { Box, styled } from '@mui/material';
+
+const StyledTabPanel = styled(Box)(({ theme }) => ({
+  paddingTop: theme.spacing(6),
+  paddingBottom: theme.spacing(6),
+}));
 
 export default function TabPanel({ value, index, containerSx, children }) {
   if (value !== index) {
     return <></>;
   }
 
-  return <Box sx={containerSx}>{children}</Box>;
+  return <StyledTabPanel sx={containerSx}>{children}</StyledTabPanel>;
 }
