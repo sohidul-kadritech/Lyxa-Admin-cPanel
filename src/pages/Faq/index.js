788d58a047e4be352b92fdb99f95d44014c52a7d
nazib-kadritech <nazib.absar@kadritech.se>
2023-02-23 15:59:58 +0600

    feat: add new styles to faq page

--- src/pages/Faq/index.js
+++ src/pages/Faq/index.js
@@ -1,32 +1,58 @@
 /* eslint-disable react/no-unstable-nested-components */
 // mui
-import { Box, Button, Modal, Paper, Stack, Tab, Tabs, Unstable_Grid2 as Grid } from '@mui/material';
-
-import { DataGrid } from '@mui/x-data-grid';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  Box,
+  Button,
+  IconButton,
+  Modal,
+  Paper,
+  Stack,
+  Tab,
+  Tabs,
+  Typography,
+  Unstable_Grid2 as Grid,
+  useTheme,
+} from '@mui/material';
 
 // third party
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 // project import
+import { faqType } from '../../assets/staticData';
 import BreadCrumbs from '../../components/BreadCrumb2';
 import CircularLoader from '../../components/CircularLoader';
+import FilterSelect from '../../components/filterSelect';
 import GlobalWrapper from '../../components/GlobalWrapper';
+import StyledGrid from '../../components/StyledGrid';
 import TabPanel from '../../components/TabPanel';
 import ThreeDotsMenu from '../../components/ThreeDotsMenu';
 import { addFaq, deleteFaq, getAllFaq, updateFaq } from '../../store/faq/faqActions';
 import AddFaq from './addFaq';
 
 export default function Faq() {
+  const theme = useTheme();
+
   const dispatch = useDispatch();
-  const { faq, loading } = useSelector((store) => store.faqReducer);
+  const { faq: faqData, loading } = useSelector((store) => store.faqReducer);
 
   const [isRightBarOpen, setIsRightBarOpen] = useState(false);
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [faq, setFaq] = useState([]);
 
   const [currentTab, setCurrentTab] = useState(0);
   const [currentFaq, setCurrentFaq] = useState({});
   const [deleteFaqId, setDeleteFaqId] = useState('');
+
+  // filters
+  const [type, setType] = useState('');
+
+  const filterType = (e) => {
+    setType(e.target.value);
+    const filteredFaqs = faqData.filter((item) => item?.type === e.target.value);
+    setFaq(filteredFaqs);
+  };
 
   // breadcrumb items
   const breadcrumbItems = [
@@ -36,7 +62,7 @@ export default function Faq() {
     },
     {
       to: '/faq',
-      label: 'Faq',
+      label: 'FAQ',
     },
   ];
 
@@ -58,6 +84,14 @@ export default function Faq() {
   // delete faq
   const callDeleteFaq = () => {
     dispatch(deleteFaq(deleteFaqId));
+  };
+
+  // dynamic titile
+  const rightBarTitle = (curTab) => {
+    if (curTab === 0) {
+      return 'Edit';
+    }
+    return 'Add New';
   };
 
   // three dot handler
@@ -131,10 +165,10 @@ export default function Faq() {
   ];
 
   useEffect(() => {
-    if (!currentFaq?._id) {
-      setCurrentFaq(faq[0] || {});
+    if (faqData?.length > 0) {
+      setFaq(faqData);
     }
-  }, [faq]);
+  }, [faqData]);
 
   useEffect(() => {
     callGetAllFaq();
@@ -143,70 +177,86 @@ export default function Faq() {
   return (
     <GlobalWrapper>
       <Box className="page-content">
-        {/* <Breadcrumb
-          maintitle="Lyxa"
-          breadcrumbItem="Faq"
-          loading={loading}
-          callList={callGetAllFaq}
-          isAddNew
-          addNewHandler={() => {
-            setIsRightBarOpen(true);
-            setCurrentTab(1);
-          }}
-        /> */}
-        <BreadCrumbs items={breadcrumbItems} />
-        <Grid container spacing={3}>
+        <Grid container sx={{ height: 'calc(100vh - 140px)' }}>
           {/* left */}
-          <Grid md={isRightBarOpen ? 6 : 12} container>
+          <Grid md={isRightBarOpen ? 8 : 12} pr={isRightBarOpen ? 3 : 0} container>
             <Grid md={12}>
-              <Paper>
-                <Stack sx={{ height: '300px', width: '100%', position: 'relative' }}>
-                  <Box sx={{ flexGrow: 1, height: '100%', width: '100%' }}>
-                    <DataGrid
-                      columns={columns}
-                      rows={faq}
-                      density="comfortable"
-                      disableColumnMenu
-                      paginationMode="server"
-                      disableSelectionOnClick
-                      hideFooterPagination
-                      getRowId={(params) => params?._id}
-                      components={{
-                        NoRowsOverlay: () => (
-                          <Stack height="100%" alignItems="center" justifyContent="center">
-                            {loading ? '' : 'No Q&A found'}
-                          </Stack>
-                        ),
-                      }}
-                    />
-                    {/* loading */}
-                    {loading ? (
-                      <Box
-                        sx={{
-                          width: '100%',
-                          height: '100%',
-                          position: 'absolute',
-                          background: 'rgba(255, 255, 255, 0.7)',
-                          left: '0',
-                          top: '0',
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          zIndex: 9,
-                        }}
-                      >
-                        <CircularLoader />
-                      </Box>
-                    ) : null}
-                  </Box>
+              <BreadCrumbs
+                items={breadcrumbItems}
+                sx={{
+                  pb: 0,
+                }}
+              />
+              <Paper
+                sx={{
+                  paddingLeft: '12px',
+                  paddingRight: '12px',
+                }}
+              >
+                <Stack direction="row" spacing={3} pt={8} pb={4} justifyContent="space-between">
+                  <FilterSelect items={faqType} placeholder="Type" value={type} onChange={filterType} />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      setIsRightBarOpen(true);
+                      setCurrentTab(1);
+                    }}
+                  >
+                    Add New
+                  </Button>
                 </Stack>
+                <Box sx={{ flexGrow: 1, height: '100%', width: '100%', position: 'relative' }}>
+                  <StyledGrid
+                    columns={columns}
+                    rows={faq}
+                    getRowId={(params) => params?._id}
+                    components={{
+                      NoRowsOverlay: () => (
+                        <Stack height="100%" alignItems="center" justifyContent="center">
+                          {loading ? '' : 'No Q&A found'}
+                        </Stack>
+                      ),
+                    }}
+                  />
+                  {/* loading */}
+                  {loading ? (
+                    <Box
+                      sx={{
+                        width: '100%',
+                        height: '100%',
+                        position: 'absolute',
+                        background: 'rgba(255, 255, 255, 0.7)',
+                        left: '0',
+                        top: '0',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        zIndex: 9,
+                      }}
+                    >
+                      <CircularLoader />
+                    </Box>
+                  ) : null}
+                </Box>
               </Paper>
             </Grid>
           </Grid>
           {/* right */}
-          <Grid container md={6} className={`${isRightBarOpen ? '' : 'd-none'}`}>
+          <Grid
+            container
+            md={4}
+            className={`${isRightBarOpen ? '' : 'd-none'}`}
+            pl={3}
+            sx={{
+              borderLeft: `1px solid ${theme.palette.grey[500]}`,
+            }}
+          >
             <Grid xs={12}>
               <Paper>
+                <Typography variant="h1" pt={5} pb={2}>
+                  {rightBarTitle(currentTab)}
+                </Typography>
                 <Stack direction="row" justifyContent="space-between" alignItems="center" pr={1}>
                   <Box>
                     {/* tab headers */}
@@ -216,39 +266,30 @@ export default function Faq() {
                         setCurrentTab(value);
                       }}
                     >
-                      <Tab label="Edit FAQ"></Tab>
-                      <Tab label="Add New"></Tab>
+                      <Tab label="Edit FAQ" className={`${currentFaq?._id ? '' : 'd-none'}`} />
+                      <Tab label="Add New" />
                     </Tabs>
                   </Box>
                   <Box>
-                    <Button
-                      variant="contained"
+                    <IconButton
+                      color="primary"
+                      sx={{
+                        color: '#000',
+                      }}
                       onClick={() => {
                         setIsRightBarOpen(false);
                       }}
                     >
-                      Close
-                    </Button>
+                      <CloseIcon />
+                    </IconButton>
                   </Box>
                 </Stack>
                 {/* tab bodies */}
                 <Box>
-                  <TabPanel
-                    index={0}
-                    value={currentTab}
-                    containerSx={{
-                      padding: '24px 16px',
-                    }}
-                  >
+                  <TabPanel index={0} value={currentTab}>
                     <AddFaq isEdit faq={currentFaq} submitHandler={callUpdateFaq} />
                   </TabPanel>
-                  <TabPanel
-                    index={1}
-                    value={currentTab}
-                    containerSx={{
-                      padding: '24px 16px',
-                    }}
-                  >
+                  <TabPanel index={1} value={currentTab}>
                     <AddFaq submitHandler={callAddFaq} />
                   </TabPanel>
                 </Box>
