6fc1747e6e827b5186c599a177d35dd2cf9a332a
nazib-kadritech <nazib.absar@kadritech.se>
2023-02-24 18:03:47 +0600

    style: fix styledTable column header focus outline

--- src/components/StyledTable.js
+++ src/components/StyledTable.js
@@ -2,47 +2,47 @@ import { styled } from '@mui/material/styles';
 import { DataGrid } from '@mui/x-data-grid';
 import * as React from 'react';
 
-function customCheckbox(theme) {
-  return {
-    '& .MuiCheckbox-root svg': {
-      width: 16,
-      height: 16,
-      backgroundColor: 'transparent',
-      border: `1px solid ${theme.palette.mode === 'light' ? '#d9d9d9' : 'rgb(67, 67, 67)'}`,
-      borderRadius: 2,
-    },
-    '& .MuiCheckbox-root svg path': {
-      display: 'none',
-    },
-    '& .MuiCheckbox-root.Mui-checked:not(.MuiCheckbox-indeterminate) svg': {
-      backgroundColor: '#1890ff',
-      borderColor: '#1890ff',
-    },
-    '& .MuiCheckbox-root.Mui-checked .MuiIconButton-label:after': {
-      position: 'absolute',
-      display: 'table',
-      border: '2px solid #fff',
-      borderTop: 0,
-      borderLeft: 0,
-      transform: 'rotate(45deg) translate(-50%,-50%)',
-      opacity: 1,
-      transition: 'all .2s cubic-bezier(.12,.4,.29,1.46) .1s',
-      content: '""',
-      top: '50%',
-      left: '39%',
-      width: 5.71428571,
-      height: 9.14285714,
-    },
-    '& .MuiCheckbox-root.MuiCheckbox-indeterminate .MuiIconButton-label:after': {
-      width: 8,
-      height: 8,
-      backgroundColor: '#1890ff',
-      transform: 'none',
-      top: '39%',
-      border: 0,
-    },
-  };
-}
+// function customCheckbox(theme) {
+//   return {
+//     '& .MuiCheckbox-root svg': {
+//       width: 16,
+//       height: 16,
+//       backgroundColor: 'transparent',
+//       border: `1px solid ${theme.palette.mode === 'light' ? '#d9d9d9' : 'rgb(67, 67, 67)'}`,
+//       borderRadius: 2,
+//     },
+//     '& .MuiCheckbox-root svg path': {
+//       display: 'none',
+//     },
+//     '& .MuiCheckbox-root.Mui-checked:not(.MuiCheckbox-indeterminate) svg': {
+//       backgroundColor: '#1890ff',
+//       borderColor: '#1890ff',
+//     },
+//     '& .MuiCheckbox-root.Mui-checked .MuiIconButton-label:after': {
+//       position: 'absolute',
+//       display: 'table',
+//       border: '2px solid #fff',
+//       borderTop: 0,
+//       borderLeft: 0,
+//       transform: 'rotate(45deg) translate(-50%,-50%)',
+//       opacity: 1,
+//       transition: 'all .2s cubic-bezier(.12,.4,.29,1.46) .1s',
+//       content: '""',
+//       top: '50%',
+//       left: '39%',
+//       width: 5.71428571,
+//       height: 9.14285714,
+//     },
+//     '& .MuiCheckbox-root.MuiCheckbox-indeterminate .MuiIconButton-label:after': {
+//       width: 8,
+//       height: 8,
+//       backgroundColor: '#1890ff',
+//       transform: 'none',
+//       top: '39%',
+//       border: 0,
+//     },
+//   };
+// }
 
 const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
   border: 0,
@@ -62,15 +62,17 @@ const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
   '& .MuiDataGrid-footerContainer': {
     display: 'none',
   },
+  '& .MuiDataGrid-columnHeader:focus-within': {
+    outline: 'none',
+  },
   '& .MuiDataGrid-cell': {
     fontSize: '17px',
-    // borderBottom: `2px solid ${theme.palette.grey[500]}`,
     borderBottom: `0px`,
-    '&:focus': {
+
+    '&:focus-within': {
       outline: 'none',
     },
   },
-  ...customCheckbox(theme),
 }));
 
 export default function StyledGrid({ ...props }) {
@@ -79,7 +81,6 @@ export default function StyledGrid({ ...props }) {
       density="comfortable"
       autoHeight
       disableColumnMenu
-      isCellEditable={false}
       disableSelectionOnClick
       hideFooterPagination
       {...props}
