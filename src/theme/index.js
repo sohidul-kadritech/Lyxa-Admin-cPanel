c236f92b4d093d0faa31a2feb3e2221d812f2af0
nazib-kadritech <121851366+nazib-kadritech@users.noreply.github.com>
2023-03-26 23:04:32 +0000

    refacfor: typography styles

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,8 +1,8 @@
 import { createTheme, ThemeProvider } from '@mui/material';
 import { useMemo } from 'react';
-import pxToRem from '../helpers/pxToRem';
 import Components from './@components';
 import GetPallete from './palllete';
+import Typography from './typography';
 
 export default function Provider({ children }) {
   const customTheme = useMemo(() => {
@@ -15,113 +15,7 @@ export default function Provider({ children }) {
     theme.components = Components(theme);
 
     // typography
-    theme.typography.h1 = {
-      [theme.breakpoints.up('xs')]: {
-        fontWeight: 600,
-        fontSize: `${pxToRem(36)}rem`,
-        lineHeight: 1.1,
-        letterSpacing: '0px',
-        color: theme.palette.text.heading,
-      },
-      [theme.breakpoints.up('lg')]: {
-        fontWeight: 700,
-        fontSize: `${pxToRem(36)}rem`,
-      },
-      [theme.breakpoints.up('xl')]: {
-        fontWeight: 700,
-        fontSize: `${pxToRem(42)}rem`,
-      },
-    };
-
-    theme.typography.h2 = {
-      [theme.breakpoints.up('xs')]: {
-        fontWeight: 600,
-        fontSize: `${pxToRem(32)}rem`,
-        lineHeight: 1.1,
-        letterSpacing: '0px',
-        color: theme.palette.text.heading,
-      },
-      [theme.breakpoints.up('lg')]: {
-        fontWeight: 700,
-        fontSize: `${pxToRem(26)}rem`,
-      },
-      [theme.breakpoints.up('xl')]: {
-        fontWeight: 600,
-        fontSize: `${pxToRem(32)}rem`,
-      },
-    };
-
-    theme.typography.h3 = {
-      fontWeight: 600,
-      fontSize: `${pxToRem(24)}rem`,
-      lineHeight: 1.1,
-      letterSpacing: '0px',
-      color: theme.palette.text.heading,
-    };
-
-    theme.typography.h4 = {
-      fontWeight: 600,
-      fontSize: `${pxToRem(20)}rem`,
-      lineHeight: `${pxToRem(24)}rem`,
-      letterSpacing: '0px',
-      color: theme.palette.text.heading,
-    };
-
-    theme.typography.h5 = {
-      fontWeight: 500,
-      fontSize: `${pxToRem(18)}rem`,
-      lineHeight: `${pxToRem(28)}rem`,
-      letterSpacing: '-2%',
-      color: theme.palette.text.heading,
-    };
-
-    theme.typography.h6 = {
-      fontWeight: 500,
-      fontSize: `${pxToRem(16)}rem`,
-      lineHeight: `${pxToRem(28)}rem`,
-      letterSpacing: '-2%',
-    };
-
-    theme.typography.body1 = {
-      lineHeight: `${pxToRem(24)}rem`,
-      fontSize: `${pxToRem(16)}rem`,
-      letterSpacing: '0px',
-      fontWeight: 400,
-
-      [theme.breakpoints.up('lg')]: {
-        lineHeight: `${pxToRem(24)}rem`,
-        fontSize: `${pxToRem(16)}rem`,
-      },
-
-      [theme.breakpoints.up('xl')]: {
-        lineHeight: `${pxToRem(24)}rem`,
-        fontSize: `${pxToRem(16)}rem`,
-      },
-    };
-
-    theme.typography.body2 = {
-      fontWeight: 500,
-      fontSize: `${pxToRem(14)}rem`,
-      lineHeight: `${pxToRem(17)}rem`,
-      letterSpacing: '0px',
-    };
-
-    theme.typography.body3 = {
-      fontWeight: 400,
-      fontSize: `${pxToRem(13)}rem`,
-      lineHeight: `${pxToRem(21)}rem`,
-      color: '#B5B5C3!important',
-
-      [theme.breakpoints.up('lg')]: {
-        fontSize: `${pxToRem(13)}rem`,
-        lineHeight: `${pxToRem(21)}rem`,
-      },
-
-      [theme.breakpoints.up('xl')]: {
-        fontSize: `${pxToRem(13)}rem`,
-        lineHeight: `${pxToRem(21)}rem`,
-      },
-    };
+    theme.typography = { ...theme.typography, ...Typography(theme) };
 
     return theme;
   }, []);
--- src/theme/typography.js
+++ src/theme/typography.js
@@ -1,64 +1,113 @@
-// import pxToRem from '../helpers/pxToRem';
+import pxToRem from '../helpers/pxToRem';
 
-// export default function GetTypography() {
-//   return {
-//     fontFamily: `'Open Sans', sans-serif`,
-//     h1: {
-//       [theme.breakpoints.up('xs')]: {
-//         fontWeight: 600,
-//         fontSize: `${pxToRem(36)}rem`,
-//         lineHeight: 1.1,
-//         letterSpacing: '0px',
-//       },
-//       [theme.breakpoints.up('lg')]: {
-//         fontWeight: 700,
-//         fontSize: `${pxToRem(36)}rem`,
-//       },
-//       [theme.breakpoints.up('xl')]: {
-//         fontWeight: 700,
-//         fontSize: `${pxToRem(42)}rem`,
-//       },
-//     },
-//     h2: {
-//       fontWeight: 700,
-//       fontSize: `${pxToRem(32)}rem`,
-//       lineHeight: 1.1,
-//       letterSpacing: '0px',
-//     },
-//     h3: {
-//       fontWeight: 600,
-//       fontSize: `${pxToRem(24)}rem`,
-//       lineHeight: 1.1,
-//       letterSpacing: '0px',
-//     },
-//     h5: {
-//       fontWeight: 500,
-//       fontSize: `${pxToRem(16)}rem`,
-//       lineHeight: 1.1,
-//       letterSpacing: '0px',
-//     },
-//     body1: {
-//       fontWeight: 400,
-//       fontSize: `${pxToRem(17)}rem`,
-//       lineHeight: 1.1,
-//       letterSpacing: '0px',
-//     },
-//     body2: {
-//       fontWeight: 400,
-//       fontSize: `${pxToRem(16)}rem`,
-//       lineHeight: 1.1,
-//       letterSpacing: '0px',
-//     },
-//     body3: {
-//       fontWeight: 400,
-//       fontSize: `${pxToRem(15)}rem`,
-//       lineHeight: 1.1,
-//       letterSpacing: '0px',
-//     },
-//     display1: undefined,
-//     display2: undefined,
-//     h4: undefined,
-//     body4: undefined,
-//     body5: undefined,
-//   };
-// }
+const Typography = (theme) => ({
+  h1: {
+    [theme.breakpoints.up('xs')]: {
+      fontWeight: 600,
+      fontSize: `${pxToRem(36)}rem`,
+      lineHeight: 1.1,
+      letterSpacing: '0px',
+      color: theme.palette.text.heading,
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontWeight: 700,
+      fontSize: `${pxToRem(36)}rem`,
+    },
+    [theme.breakpoints.up('xl')]: {
+      fontWeight: 700,
+      fontSize: `${pxToRem(42)}rem`,
+    },
+  },
+
+  h2: {
+    [theme.breakpoints.up('xs')]: {
+      fontWeight: 600,
+      fontSize: `${pxToRem(32)}rem`,
+      lineHeight: 1.1,
+      letterSpacing: '0px',
+      color: theme.palette.text.heading,
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontWeight: 700,
+      fontSize: `${pxToRem(26)}rem`,
+    },
+    [theme.breakpoints.up('xl')]: {
+      fontWeight: 600,
+      fontSize: `${pxToRem(32)}rem`,
+    },
+  },
+
+  h3: {
+    fontWeight: 600,
+    fontSize: `${pxToRem(24)}rem`,
+    lineHeight: 1.1,
+    letterSpacing: '0px',
+    color: theme.palette.text.heading,
+  },
+
+  h4: {
+    fontWeight: 600,
+    fontSize: `${pxToRem(20)}rem`,
+    lineHeight: `${pxToRem(24)}rem`,
+    letterSpacing: '0px',
+    color: theme.palette.text.heading,
+  },
+
+  h5: {
+    fontWeight: 500,
+    fontSize: `${pxToRem(18)}rem`,
+    lineHeight: `${pxToRem(28)}rem`,
+    letterSpacing: '-2%',
+    color: theme.palette.text.heading,
+  },
+
+  h6: {
+    fontWeight: 500,
+    fontSize: `${pxToRem(16)}rem`,
+    lineHeight: `${pxToRem(28)}rem`,
+    letterSpacing: '-2%',
+  },
+
+  body1: {
+    lineHeight: `${pxToRem(24)}rem`,
+    fontSize: `${pxToRem(16)}rem`,
+    letterSpacing: '0px',
+    fontWeight: 400,
+
+    [theme.breakpoints.up('lg')]: {
+      lineHeight: `${pxToRem(24)}rem`,
+      fontSize: `${pxToRem(16)}rem`,
+    },
+
+    [theme.breakpoints.up('xl')]: {
+      lineHeight: `${pxToRem(24)}rem`,
+      fontSize: `${pxToRem(16)}rem`,
+    },
+  },
+
+  body2: {
+    fontWeight: 500,
+    fontSize: `${pxToRem(14)}rem`,
+    lineHeight: `${pxToRem(17)}rem`,
+    letterSpacing: '0px',
+  },
+
+  body3: {
+    fontWeight: 400,
+    fontSize: `${pxToRem(13)}rem`,
+    lineHeight: `${pxToRem(21)}rem`,
+    color: '#B5B5C3!important',
+
+    [theme.breakpoints.up('lg')]: {
+      fontSize: `${pxToRem(13)}rem`,
+      lineHeight: `${pxToRem(21)}rem`,
+    },
+
+    [theme.breakpoints.up('xl')]: {
+      fontSize: `${pxToRem(13)}rem`,
+      lineHeight: `${pxToRem(21)}rem`,
+    },
+  },
+});
+
+export default Typography;
