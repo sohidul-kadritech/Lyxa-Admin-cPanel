7f35b906bac62fa0aff633515bff421f264d5686
nazib-kadritech <nazib.absar@kadritech.se>
2023-04-17 11:34:58 +0600

    style: add global styles for tooltip

--- src/theme/@components/Tooltip.js
+++ src/theme/@components/Tooltip.js
@@ -4,6 +4,15 @@ export default function Tooltip() {
       popper: {
         zIndex: 1200,
       },
+      tooltip: {
+        fontSize: '12px',
+        lineHeight: '20px',
+        fontWeight: '400',
+        fontFamily: 'Inter',
+        color: '#fff',
+        backgroundColor: '#404040',
+        maxWidth: '200px',
+      },
     },
   };
 }
