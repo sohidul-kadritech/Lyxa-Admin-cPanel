86a2331787b754d1715c415248598a7c48697cd7
nazib-kadritech <nazib.absar@kadritech.se>
2023-05-12 16:32:59 +0600

    add new global sidebar

--- src/components/Layout/Topbar/index.js
+++ src/components/Layout/Topbar/index.js
@@ -1,13 +1,12 @@
 import MenuIcon from '@mui/icons-material/Menu';
-import { Avatar, Box, Button, IconButton, Stack, Typography, useMediaQuery } from '@mui/material';
+import { Avatar, Box, Button, IconButton, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
+import { ReactComponent as Logo } from '../../../assets/icons/logo-sm.svg';
 import { ReactComponent as NotificationIcon } from '../../../assets/icons/t-notification.svg';
 import { ReactComponent as SupportIcon } from '../../../assets/icons/t-support.svg';
 import AccountMenu from './AccountMenu';
 
-export default function Topbar({ setSidebar }) {
-  const matches = useMediaQuery('(max-width: 1100px)');
-
+export default function Topbar({ setSidebar, sidebar }) {
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -27,23 +26,31 @@ export default function Topbar({ setSidebar }) {
       pl="30px"
       pr="30px"
       sx={{
-        borderBottom: '1px solid #F8F8F8',
+        zIndex: '1500',
+        background: '#fff',
+        borderBottom: `1px solid ${sidebar ? 'rgba(177, 177, 177, 0.2)' : 'transparent'}`,
       }}
     >
-      <Box>
-        {matches && (
+      <Stack direction="row" alignItems="center" gap={2}>
+        <Box>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={() => {
-              setSidebar(true);
+              setSidebar((prev) => !prev);
             }}
             edge="start"
           >
             <MenuIcon />
           </IconButton>
-        )}
-      </Box>
+        </Box>
+        <Stack direction="row" alignItems="center" gap={2}>
+          <Logo />
+          <Typography variant="inherit" fontSize={22} lineHeight="26px" fontWeight={500}>
+            Sales Manager
+          </Typography>
+        </Stack>
+      </Stack>
       <Stack direction="row" alignItems="center" gap="25px">
         <Typography variant="body2">
           <SupportIcon /> Get Support
