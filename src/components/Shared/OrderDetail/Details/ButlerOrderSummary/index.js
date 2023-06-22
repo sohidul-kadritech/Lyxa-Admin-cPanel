bac66307e4f296cdd9cf58f6ad9e4f8d22b8a7f8
nazib-kadritech <nazib.absar@kadritech.se>
2023-06-22 20:00:10 +0600

    feat: add order for butler order detail

--- src/components/Shared/OrderDetail/Details/ButlerOrderSummary/index.js
+++ src/components/Shared/OrderDetail/Details/ButlerOrderSummary/index.js
@@ -1,14 +1,17 @@
-import { Modal, Stack, Typography } from '@mui/material';
+import { Box, Modal, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
+import { useGlobalContext } from '../../../../../context';
 import EditDocument from '../../../../Common/EditDocument';
 import { StyledOrderDetailBox } from '../../helpers';
 import ButlerProduct from './Product';
 
 export default function ButlerOrderSummary({ order }) {
+  const quantity = order?.products?.reduce((acc, cur) => acc + cur.quantity, 0) || null;
   const [editDocumentOpen, setEditDocumentOpen] = useState(false);
   const [currentDoc, setCurrentDoc] = useState({});
 
-  const quantity = order?.products?.reduce((acc, cur) => acc + cur.quantity, 0) || null;
+  const { general } = useGlobalContext();
+  const currency = general?.currency?.symbol;
 
   const onImagePreview = (imgUrl) => {
     setEditDocumentOpen(true);
@@ -37,11 +40,21 @@ export default function ButlerOrderSummary({ order }) {
           </Typography>
         )}
         {order?.orderType === 'purchase_delivery' && (
-          <Stack gap={3} pt={2}>
-            {order?.products?.map((product, index) => (
-              <ButlerProduct product={product} key={index} onImagePreview={onImagePreview} />
-            ))}
-          </Stack>
+          <Box>
+            <Stack gap={3} pt={2} pb={5}>
+              {order?.products?.map((product, index) => (
+                <ButlerProduct product={product} key={index} onImagePreview={onImagePreview} />
+              ))}
+            </Stack>
+            <Typography variant="body2" color="textPrimary" lineHeight="22px">
+              Note: Estimated total will be{' '}
+              <span style={{ fontWeight: 'bold' }}>
+                {currency}
+                {order?.summary?.productAmount}
+              </span>
+              . This amount will be paid by <span style={{ fontWeight: 'bold' }}>cash</span>.
+            </Typography>
+          </Box>
         )}
       </StyledOrderDetailBox>
       <Modal open={editDocumentOpen} onClose={() => setEditDocumentOpen(false)}>
