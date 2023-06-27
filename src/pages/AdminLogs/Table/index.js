3c5574409f8adf41e7a9773d3b04c9ec54aca84f
sohidul-kadritech <sohidul.islam@kadritech.se>
2023-06-27 18:04:24 +0600

    add units type in admin logs

--- src/pages/AdminLogs/Table/index.js
+++ src/pages/AdminLogs/Table/index.js
@@ -1,10 +1,9 @@
-import { Box, Stack, Typography, useTheme } from '@mui/material';
-import { isNaN } from 'lodash';
+import { Box, Stack, useTheme } from '@mui/material';
 import React from 'react';
 import TablePagination from '../../../components/Common/TablePagination';
 import StyledTable from '../../../components/Styled/StyledTable3';
 import { useGlobalContext } from '../../../context';
-import { allColumns } from './helpers';
+import { allColumns, getValue } from './helpers';
 
 function AdminLogsTable({ data, loading, queryParams, setQueryParams, totalPage }) {
   const theme = useTheme();
@@ -12,129 +11,6 @@ function AdminLogsTable({ data, loading, queryParams, setQueryParams, totalPage 
   const { general } = useGlobalContext();
 
   const { currency } = general;
-
-  const getValue = (type, value) => {
-    let newValue = null;
-    if (!value || value.length <= 0) {
-      newValue = 0;
-    } else if (type === 'currency') {
-      newValue = `${value?.code} (${value?.symbol})`;
-    } else if (type === 'adminCutForReward') {
-      newValue = value;
-    } else if (type === 'minSpendLimit') {
-      newValue = value;
-    } else if (type === 'expiration_period') {
-      newValue = value;
-    } else if (type === 'rewardCategory') {
-      newValue = (
-        <Stack gap="4px" padding="8px 0px">
-          {value.map((item) => (
-            <Typography variant="body4" key={item}>
-              {item.name}
-            </Typography>
-          ))}
-        </Stack>
-      );
-    } else if (type === 'redeemReward') {
-      newValue = (
-        <Stack gap="4px" padding="16px 0px">
-          <Typography variant="body4">
-            amount: {currency?.symbol}
-            {value.amount}
-          </Typography>
-          <Typography variant="body4">points: {value.points}</Typography>
-        </Stack>
-      );
-    } else if (type === 'getReward') {
-      newValue = (
-        <Stack gap="4px" padding="16px 0px">
-          <Typography variant="body4">
-            amount: {currency?.symbol}
-            {value.amount}
-          </Typography>
-          <Typography variant="body4">points: {value.points}</Typography>
-        </Stack>
-      );
-    } else if (type === 'maxCustomerServiceValue' || type === 'maxTotalEstItemsPriceForButler') {
-      newValue = `${currency?.symbol}${value}`;
-    } else if (type === 'nearByShopKm' || type === 'nearByShopKmForUserHomeScreen' || type === 'maxDistanceForButler') {
-      newValue = `${value}km`;
-    } else if (type === 'rewardBundle') {
-      newValue = (
-        <Stack gap="4px" padding="8px 0px">
-          {value.map((item) => (
-            <Typography variant="body4" key={item}>
-              {item}
-            </Typography>
-          ))}
-        </Stack>
-      );
-    } else if (type === 'maxDiscount') {
-      newValue = (
-        <Stack gap="4px" padding="16px 0px">
-          {value.map((item) => (
-            <Typography variant="body4" key={item}>
-              {item}%
-            </Typography>
-          ))}
-        </Stack>
-      );
-    } else if (type === 'searchDeliveryBoyKm') {
-      newValue = (
-        <Stack gap="4px" padding="16px 0px">
-          {value.map((item) => (
-            <Typography variant="body4" key={item}>
-              {item} km
-            </Typography>
-          ))}
-        </Stack>
-      );
-    } else if (type === 'specificSellerDropCharge') {
-      newValue = (
-        <Typography variant="body4">{`${value?.value?.dropPercentage ?? 0}${'%'} ${
-          value?.seller?.company_name ? `(${value?.seller?.company_name})` : ''
-        }`}</Typography>
-      );
-    } else if (type === 'globalDropCharge' || type === 'sellerDropChargeReset') {
-      newValue = (
-        <Typography variant="body4">{`${value?.dropPercentage ?? 0}${
-          '%'
-          // value?.dropPercentageType === 'parcentage' ? '%' : currency?.symbol
-        }`}</Typography>
-      );
-    } else if (
-      type === 'globalDeliveryCut' ||
-      type === 'specificSellerDeliveryCut' ||
-      type === 'globalDeliveryCutForButler'
-    ) {
-      newValue = (
-        <Stack gap="4px" padding="16px 0px">
-          {value.map((item) => (
-            <Typography
-              key={`${item?.from}_${item?.to}`}
-              variant="body4"
-              // eslint-disable-next-line max-len
-            >{`(${item?.from} - ${item?.to} km) - charge: ${currency?.symbol}${item?.charge} rider: ${currency?.symbol}${item?.deliveryPersonCut}`}</Typography>
-          ))}
-        </Stack>
-      );
-    } else if (
-      (typeof value === 'string' || typeof value === 'number') &&
-      value !== undefined &&
-      !isNaN(value) &&
-      value !== null &&
-      value !== 'undefined' &&
-      value !== 'null'
-    ) {
-      console.log('value:', value, ' type: ', typeof value);
-      if (value) {
-        newValue = value;
-      } else newValue = 0;
-    } else {
-      newValue = 0;
-    }
-    return newValue;
-  };
 
   return (
     <>
@@ -154,7 +30,7 @@ function AdminLogsTable({ data, loading, queryParams, setQueryParams, totalPage 
           }}
         >
           <StyledTable
-            columns={allColumns(getValue)}
+            columns={allColumns(getValue, currency)}
             rows={data || []}
             getRowId={(row) => row?._id}
             getRowHeight={() => 'auto'}
