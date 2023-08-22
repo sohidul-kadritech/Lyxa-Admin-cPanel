73b862f8cd4bb0b4934f241183d3d0f9145ee1c3
nazib-kadritech <nazib.absar@kadritech.se>
2023-08-22 18:49:50 +0600

    feat: add date range presets in date filter

---
+++ src/components/Styled/StyledDateRangePicker/index.js
@@ -0,0 +1,34 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import _ from 'lodash';
+import React, { useState } from 'react';
+import { DateRangePicker } from 'react-dates';
+import DatePresets from './Presets';
+
+// eslint-disable-next-line no-unused-vars
+export default function StyledDateRangePicker({ startDate, endDate, onChange }) {
+  const [focusedInput, setFocusedInput] = useState(null);
+
+  return (
+    <DateRangePicker
+      // startDate={moment(startDate)}
+      // endDate={moment(endDate)}
+      startDate={startDate}
+      endDate={endDate}
+      startDateId={_.uniqueId('start_date_id')}
+      endDateId={_.uniqueId('end_date_id')}
+      onDatesChange={onChange}
+      focusedInput={focusedInput}
+      onFocusChange={(focusedInput) => setFocusedInput(focusedInput)}
+      noBorder
+      date
+      customArrowIcon="-"
+      showDefaultInputIcon
+      inputIconPosition="after"
+      customInputIcon={<ExpandMoreIcon />}
+      hideKeyboardShortcutsPanel
+      keepOpenOnDateSelect
+      isOutsideRange={() => false}
+      renderCalendarInfo={() => <DatePresets endDate={endDate} startDate={startDate} onChange={onChange} />}
+    />
+  );
+}
