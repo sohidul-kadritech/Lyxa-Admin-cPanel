f70655bb1f09bc18a2391a35cf8caa8f928faaa6
nazib-kadritech <nazib.absar@kadritech.se>
2023-09-23 15:33:18 +0600

    product list persist search params

--- src/pages/Product1/index.js
+++ src/pages/Product1/index.js
@@ -1,17 +1,16 @@
-import { Box, Stack, Tab, Tabs } from '@mui/material';
-import React, { useState } from 'react';
+import { Box, Tab, Tabs } from '@mui/material';
+import React from 'react';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
+import SearchBar from '../../components/Common/CommonSearchbar';
 import PageTop from '../../components/Common/PageTop';
-import StyledFormField from '../../components/Form/StyledFormField';
-import StyledSearchBar from '../../components/Styled/StyledSearchBar';
 import { useGlobalContext } from '../../context';
 import { successMsg } from '../../helpers/successMsg';
+import useQueryParams from '../../helpers/useQueryParams';
 import * as API_URL from '../../network/Api';
 import AXIOS from '../../network/axios';
-import { dateRangeInit, sortOptions } from '../Faq2/helpers';
 import ProductList from './ProductList';
 import ProductPageSkeleton from './ProductPageSkeleton';
-import { shopType, statusTypeOptions } from './helpers';
+import { shopType } from './helpers';
 
 const breadcrumbItems = [
   {
@@ -24,42 +23,26 @@ const breadcrumbItems = [
   },
 ];
 
+export const queryParamsInit = {
+  currentTab: 0,
+  status: 'active',
+  sortBy: 'DESC',
+  searchKey: '',
+};
+
 function Product() {
   const { currentUser } = useGlobalContext();
-  console.log(currentUser);
-  // eslint-disable-next-line no-unused-vars
-  const [currentTab, setCurrentTab] = useState(0);
-  // eslint-disable-next-line no-unused-vars
-  const [range, setRange] = useState({ ...dateRangeInit });
-  // eslint-disable-next-line no-unused-vars
-  const [status, setStatus] = useState('active');
-  const [sort, setSort] = useState('asc');
-  // eslint-disable-next-line no-unused-vars
-  const [type, setType] = useState('food');
-  // eslint-disable-next-line no-unused-vars
-  const [open, setOpen] = useState(false);
-  const [searchKey, setSearchKey] = useState('');
+  const [queryParams, setQueryParams] = useQueryParams(queryParamsInit);
 
   const queryClient = useQueryClient();
   const url = currentUser.userType === 'admin' ? API_URL.ALL_PRODUCT : '';
 
-  const getAllProduct = useQuery(
-    [url, { status, type, sort, searchKey, startDate: range.start, endDate: range.end }],
-    () =>
-      AXIOS.get(url, {
-        params: {
-          status,
-          sort,
-          type,
-          searchKey,
-          startDate: range.start,
-          endDate: range.end,
-        },
-        // eslint-disable-next-line prettier/prettier
-      }),
+  const getAllProduct = useQuery([url, queryParams], () =>
+    AXIOS.get(url, {
+      params: queryParams,
+    })
   );
 
-  // eslint-disable-next-line no-unused-vars
   const updateStatusQuery = useMutation((data) => AXIOS.post(API_URL.EDIT_PRODUCT, data), {
     onSuccess: (data) => {
       if (data.status) {
@@ -69,9 +52,6 @@ function Product() {
     },
   });
 
-  console.log('data', getAllProduct?.data?.data?.products);
-
-  // const getCurrentCurrency = JSON.parse(localStorage.getItem('currency'));
   const { general } = useGlobalContext();
   const getCurrentCurrency = general?.currency;
 
@@ -95,13 +75,9 @@ function Product() {
 
       <Box marginBottom="20px">
         <Tabs
-          value={currentTab}
+          value={Number(queryParams?.currentTab)}
           onChange={(event, newValue) => {
-            setCurrentTab(() => {
-              setType(shopType[newValue]);
-              return newValue;
-            });
-            // setIsSideBarOpen(false);
+            setQueryParams((prev) => ({ ...prev, type: shopType[newValue], currentTab: newValue }));
           }}
         >
           <Tab label="Food"></Tab>
@@ -111,40 +87,7 @@ function Product() {
       </Box>
 
       <Box marginBottom="30px">
-        <Stack direction="row" justifyContent="start" gap="17px" sx={{ marginBottom: '30px' }}>
-          <StyledSearchBar sx={{ flex: '1' }} placeholder="Search" onChange={(e) => setSearchKey(e.target.value)} />
-          <StyledFormField
-            intputType="select"
-            containerProps={{
-              sx: { padding: '0px 0px' },
-            }}
-            inputProps={{
-              name: 'sort',
-              placeholder: 'Sort',
-              value: sort,
-              items: sortOptions,
-              size: 'sm2',
-              //   items: categories,
-              onChange: (e) => setSort(e.target.value),
-            }}
-          />
-          <StyledFormField
-            intputType="select"
-            containerProps={{
-              sx: { padding: '0px 0px' },
-            }}
-            inputProps={{
-              name: 'status',
-              placeholder: 'Status',
-              value: status,
-              items: statusTypeOptions,
-              size: 'sm2',
-              //   items: categories,
-              onChange: (e) => setStatus(e.target.value),
-            }}
-          />
-          {/* <AddMenuButton onClick={() => setOpen(true)} /> */}
-        </Stack>
+        <SearchBar queryParams={queryParams} setQueryParams={setQueryParams} />
       </Box>
       {getAllProduct?.isLoading ? (
         <Box
