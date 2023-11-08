e15226c4de56b340f90062b88326cfc2ababbf25
sohidul-kadritech <sohidul.islam@kadritech.se>
2023-11-08 18:53:45 +0600

    added action taken in customer service and admin order table

--- src/components/Shared/ChatDetail/Chat/ChatIssue.js
+++ src/components/Shared/ChatDetail/Chat/ChatIssue.js
@@ -5,17 +5,19 @@ import { StyledOrderDetailBox } from '../../OrderDetail/helpers';
 export default function ChatIssues({ chat }) {
   const theme = useTheme();
 
+  console.log('chat issue', { chat });
+
   return (
     <StyledOrderDetailBox
       title={
         <span>
-          <FlagIcon style={{ color: theme.palette.error.main, marginRight: '3px' }} /> Issue{' '}
+          <FlagIcon style={{ color: theme.palette.error.main, marginRight: '3px' }} /> Action Taken{' '}
           <span style={{ textTransform: 'capitalize' }}>({chat?.chatType})</span>
         </span>
       }
     >
       <Typography variant="body2" fontSize={12} lineHeight="22px" pt={1}>
-        {chat?.reasonMessage}
+        {chat?.resolvedReason?.resolveReason}
       </Typography>
     </StyledOrderDetailBox>
   );
