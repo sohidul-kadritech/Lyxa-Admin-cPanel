1569044de65cbf2702fbd5d811d03388bcb822ba
sohidul-kadritech <sohidul.islam@kadritech.se>
2023-11-09 15:12:24 +0600

    Shop settings rate change to exchange rate and field value should be smooth

--- src/components/Form/IncrementDecrementInput.js
+++ src/components/Form/IncrementDecrementInput.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-unsafe-optional-chaining */
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { Button, Input, Stack, Typography, styled } from '@mui/material';
+import { useEffect } from 'react';
 
 const StyledContainer = styled(Stack)(({ theme }) => ({
   background: theme.palette.background.secondary,
@@ -12,6 +14,7 @@ const StyledContainer = styled(Stack)(({ theme }) => ({
 
 export default function IncrementDecrementInput({
   onChange,
+  onBlur,
   allowDecimal = true,
   endAdornment,
   dynamicWidth,
@@ -21,6 +24,31 @@ export default function IncrementDecrementInput({
   step = 1,
 }) {
   const length = 190 + Math.max(value?.toString().length - 1, 0) * 8;
+
+  useEffect(() => {
+    // Function to handle mouseMove event
+    const handleMouseMove = (event) => {
+      // Do something with the mouse position
+      const newValue = allowDecimal ? value : Math.round(Number(value));
+      if (onBlur) {
+        if (Number(newValue) < min) {
+          onBlur(min);
+        } else if (Number(newValue) > max) {
+          onBlur(max);
+        } else {
+          onBlur(Number(newValue));
+        }
+      }
+    };
+
+    // Add event listener when the component mounts
+    document.addEventListener('mousemove', handleMouseMove);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
   return (
     <Stack direction="row" alignItems="center" gap={3}>
@@ -57,14 +85,48 @@ export default function IncrementDecrementInput({
           }}
           type="number"
           value={value}
+          onBlur={(e) => {
+            const value = allowDecimal ? e.target.value : Math.round(Number(e.target.value));
+            if (onBlur) {
+              if (Number(value) < min) {
+                onBlur(min, e);
+              } else if (Number(value) > max) {
+                onBlur(max, e);
+              } else {
+                onBlur(Number(value), e);
+              }
+            }
+          }}
+          onMouseMove={(e) => {
+            const newValue = allowDecimal ? value : Math.round(Number(value));
+            if (onBlur) {
+              if (Number(newValue) < min) {
+                onBlur(min, e);
+              } else if (Number(newValue) > max) {
+                onBlur(max, e);
+              } else {
+                onBlur(Number(newValue), e);
+              }
+            }
+          }}
           onChange={(e) => {
             const value = allowDecimal ? e.target.value : Math.round(Number(e.target.value));
-            if (Number(value) < min) {
-              onChange(min, e);
-            } else if (Number(value) > max) {
-              onChange(max, e);
-            } else {
-              onChange(Number(value), e);
+
+            console.log({ input: value });
+
+            if (onChange && !onBlur) {
+              if (Number(value) < min) {
+                onChange(min, e);
+              } else if (Number(value) > max) {
+                onChange(max, e);
+              } else {
+                onChange(Number(value), e);
+              }
+            }
+
+            if (onChange && onBlur) {
+              const newValue = min > 0 ? (value > 0 ? value : 1) : 0;
+              onChange(newValue, e);
             }
           }}
         />
