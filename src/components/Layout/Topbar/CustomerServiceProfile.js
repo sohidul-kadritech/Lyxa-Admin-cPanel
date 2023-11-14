e50edde884306e709f6ca1eefe875d9298adbf3e
sohidul-kadritech <sohidul.islam@kadritech.se>
2023-11-14 13:03:10 +0600

    change cs profile style

--- src/components/Layout/Topbar/CustomerServiceProfile.js
+++ src/components/Layout/Topbar/CustomerServiceProfile.js
@@ -1,14 +1,19 @@
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable no-unused-vars */
-import { Email, Logout } from '@mui/icons-material';
-import { Box, Stack, Typography, useTheme } from '@mui/material';
-import React from 'react';
+import { Edit, Email, Logout } from '@mui/icons-material';
+import { Avatar, Box, Drawer, Stack, Typography, useTheme } from '@mui/material';
+import React, { useState } from 'react';
 import { ReactComponent as Phone } from '../../../assets/icons/phone.svg';
 
 import { ReactComponent as UserIcon } from '../../../assets/icons/user_outline.svg';
 import socketServices from '../../../common/socketService';
+import { useGlobalContext } from '../../../context';
 import getCookiesAsObject from '../../../helpers/cookies/getCookiesAsObject';
 import setCookiesAsObject from '../../../helpers/cookies/setCookiesAsObject';
 import CustomerServiceOnlineOfflineToggle from '../../../pages/OngoingTickets/CustomerServiceOnlineOfflineToggle';
+import EditAdminProfile from '../../../pages/SalesManagerDashBoard/EditAdminProfile';
+import StyledIconButton from '../../Styled/StyledIconButton';
+import { getProfilePhotoAndAltName } from '../helper';
 
 function CustomerServiceInfoBox({ icon, label, onClickHandler }) {
   return (
@@ -55,6 +60,13 @@ function CustomerServiceProfileCardContainer({ children, theme, sx }) {
 
 function CustomerServiceProfile({ admin, open, onClose }) {
   const theme = useTheme();
+
+  const [openEdit, setOpenEdit] = useState(false);
+
+  const { currentUser } = useGlobalContext();
+
+  const { profilePhoto, altName } = getProfilePhotoAndAltName(currentUser, currentUser?.userType);
+
   const onClickHandler = (href) => {
     window.location.href = href;
   };
@@ -76,7 +88,7 @@ function CustomerServiceProfile({ admin, open, onClose }) {
         borderRadius: '10px',
         minWidth: '300px',
         minHeight: open ? '100px' : '0px',
-        maxHeight: open ? '250px' : '0px',
+        maxHeight: open ? '350px' : '0px',
         background: '#fff',
         overflow: 'auto',
         border: `1px solid ${theme?.palette?.custom?.border}`,
@@ -93,6 +105,53 @@ function CustomerServiceProfile({ admin, open, onClose }) {
           width: '100%',
         }}
       >
+        <CustomerServiceProfileCardContainer
+          theme={theme}
+          sx={{
+            '&:hover': {
+              backgroundColor: 'rgba(177, 177, 177, 0.0)',
+            },
+          }}
+        >
+          <Stack justifyContent="center" alignItems="center">
+            <Box sx={{ position: 'relative', top: 0 }}>
+              <Avatar src={profilePhoto} alt="profile_photo" sx={{ width: '70px', height: '70px' }}>
+                {altName}
+              </Avatar>
+              <Box sx={{ position: 'absolute', bottom: 4, right: 4 }}>
+                <StyledIconButton
+                  sx={{
+                    backgroundColor: '#E4E6EB',
+                    padding: '9px 9px 11px 9px',
+                    borderRadius: '50%!important',
+                    width: '28px',
+                    height: '28px',
+                    '&:hover': {
+                      backgroundColor: 'background.secondaryHover',
+                    },
+                  }}
+                  onClick={() => {
+                    setOpenEdit(true);
+                  }}
+                >
+                  <Edit />
+                </StyledIconButton>
+              </Box>
+              {/* <Box sx={{ position: 'absolute', top: -4, left: 4 }}>
+                <Tooltip title={<span style={{ textTransform: 'capitalize' }}>{admin?.liveStatus}</span>}>
+                  <Box
+                    sx={{
+                      width: '18px',
+                      height: '18px',
+                      borderRadius: '50%',
+                      background: admin?.liveStatus === 'online' ? statusColor?.green : statusColor?.orange,
+                    }}
+                  />
+                </Tooltip>
+              </Box> */}
+            </Box>
+          </Stack>
+        </CustomerServiceProfileCardContainer>
         <CustomerServiceProfileCardContainer theme={theme}>
           <CustomerServiceInfoBox icon={<UserIcon />} label={admin?.name} />
         </CustomerServiceProfileCardContainer>
@@ -137,10 +196,32 @@ function CustomerServiceProfile({ admin, open, onClose }) {
           />
         </CustomerServiceProfileCardContainer>
 
-        <CustomerServiceProfileCardContainer theme={theme}>
+        {/* edit profile */}
+        {/* <CustomerServiceProfileCardContainer theme={theme}>
+          <CustomerServiceInfoBox icon={<Edit />} onClickHandler={() => setOpenEdit(true)} label="Edit Profile" />
+        </CustomerServiceProfileCardContainer> */}
+
+        {/* logout */}
+        <CustomerServiceProfileCardContainer
+          theme={theme}
+          sx={{
+            cursor: 'pointer !important',
+          }}
+        >
           <CustomerServiceInfoBox icon={<Logout />} onClickHandler={_logout} label="Logout" />
         </CustomerServiceProfileCardContainer>
       </Stack>
+
+      <Drawer anchor="right" open={openEdit}>
+        <EditAdminProfile
+          isEdit
+          currentAdmin={admin}
+          onClose={() => {
+            setOpenEdit(false);
+          }}
+          adminType="customerService"
+        />
+      </Drawer>
     </Box>
   );
 }
